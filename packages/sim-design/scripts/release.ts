3cc79b4f326d40d9ffe024da91eb5491fb0b0675
laoer536 <1547698569@qq.com>
2023-06-13 19:47:53 +0800

    feat: build优化

---
+++ packages/sim-design/scripts/release.ts
@@ -0,0 +1,27 @@
+import minimist from 'minimist'
+import { writeFileSync, readFileSync } from 'fs'
+import consola from 'consola'
+interface PackageJsonInfo {
+  main: string
+  module: string
+  [k: string]: string
+}
+
+type Mode = 'pre' | 'after'
+const mode = minimist(process.argv.slice(2)).mode as Mode
+setEntrance(mode)
+function setEntrance(mode: Mode) {
+  consola.info(`setEntranceMode:${mode}`)
+  const packageJson = JSON.parse(readFileSync('./package.json', 'utf8')) as PackageJsonInfo
+  if (mode == 'pre') {
+    packageJson.main = 'dist/vite-lib.umd.js'
+    packageJson.module = 'dist/vite-lib.es.js'
+  } else if (mode === 'after') {
+    packageJson.main = 'index.ts'
+    packageJson.module = 'index.js'
+  } else {
+    consola.error(new Error('Can not resolve this mode'))
+    return
+  }
+  writeFileSync('./package.json', JSON.stringify(packageJson, null, 2))
+}
