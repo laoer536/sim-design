d2086169ef272d07ee91f70b561c8db66ddba75b
laoer536 <1547698569@qq.com>
2023-06-14 00:05:41 +0800

    style: code优化

--- packages/sim-design/vite.config.ts
+++ packages/sim-design/vite.config.ts
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'node:url'
 import { dirname } from 'node:path'
 const fileName = fileURLToPath(import.meta.url)
 const dirName = dirname(fileName)
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [vue(), dts()],
   resolve: {
@@ -14,19 +14,16 @@ export default defineConfig({
     //   '@compo': resolve(__dirname, './hooks'),
     // },
   },
-  // 打包配置
   build: {
     lib: {
-      entry: dirName + '/index.ts', // 设置入口文件
-      name: 'sim-design', // 起个名字，安装、引入用
-      fileName: (format) => `vite-lib.${format}.js`, // 打包后的文件名
+      entry: dirName + '/index.ts',
+      name: 'sim-design',
+      fileName: (format) => `vite-lib.${format}.js`,
     },
-    sourcemap: true, // 输出.map文件
+    sourcemap: true,
     rollupOptions: {
-      // 确保外部化处理那些你不想打包进库的依赖
       external: ['vue'],
       output: {
-        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
           vue: 'Vue',
         },
